e38680dbb2e3fbf020407c0b4df2858ddf1f3c20
Vanonymous <seanhxx42@live.com>
2017-10-25 23:58:18 +0800

    adding image crawler

---
+++ practice/image_crawler/imageFilter.py
@@ -0,0 +1,33 @@
+# a = document.querySelectorAll('img')
+# document.body.innerText = Array.prototype.map.call(a,x=>x.currentSrc)
+
+import os
+import re
+import urllib.request
+
+root_path = os.path.abspath('/home/seanhxx/Documents/images/')
+object_class = 'car'
+object_path = 'img_'+object_class
+
+url_path = os.path.join(root_path, object_path, object_class+'URL.txt')
+
+result = []
+with open(url_path, 'r') as file:
+    while True:
+        line = file.read(1024)
+        if not line:
+            break
+        a = line.split(',')
+        for i in a:
+            item = re.match('^https', i)
+            if item is not None:
+                result.append(i.strip())
+n=0
+for url in result:
+    try:
+        figure_name = ''.join([object_class, '_fig_', str(n), '.jpg'])
+        figure_path = os.path.join(root_path, object_path,figure_name)
+        urllib.request.urlretrieve(url,figure_path)
+        n += 1
+    except:
+        pass
